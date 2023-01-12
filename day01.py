e4f94a011e3a3f82311814a9fd98bea8151db407
uminsong1512 <uminsong1512@gmail.com>
2023-01-12 18:01:04 +0900

    temperature 01

--- day01.py
+++ day01.py
@@ -1,14 +1,9 @@
-# for countdown in 5,4,3,2,1,"hey":
-#     print(countdown)
-# print("프로그램 종료")
-# 단어_단어 : snake 표기법
-countdown_list=[5,4,3,2,1,"hey"]
-for countdown in countdown_list:
-    print(countdown)
-print(countdown_list[3]) #2
-print(countdown_list[-3]) #2
-print(countdown_list[-1]) #역방향 인덱싱
-print("프로그램 종료")
+# (100°F − 32) × 5/9 = 37.778°C
+fahrenheit = float(input("화씨 온도 :")) # 변수 이름은 의미를 가진 것으로 설정하는 것이 중요하다.
+celsius = (fahrenheit - 32.0) * (5.0/9.0)
+print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다.') #f' ' 문자열
+# 실행 단축키 control + R
+
 
 
 
