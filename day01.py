7884eda4f00cec199420fb454f262fbf0582cd19
uminsong1512 <uminsong1512@gmail.com>
2023-01-12 18:02:14 +0900

    subject 03

--- day01.py
+++ day01.py
@@ -1,29 +1,13 @@
-# (100°F − 32) × 5/9 = 37.778°C
-fahrenheit = float(input("화씨 온도 :")) # 변수 이름은 의미를 가진 것으로 설정하는 것이 중요하다.
-celsius = (fahrenheit - 32.0) * (5.0/9.0)
-print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다.') #f' ' 문자열
-# 실행 단축키 control + R
+#교재 딕셔너리 예제
+# 키값을 통해 indexing한다.
+subjects = {"의사소통영어":"A+","오래된 미래":"B+","양자역햑": "A0"}
+student = "김도훈"
+subject = "오래된 미래"
+print(student,"학생의",subject,"과목 성적은",subjects["오래된 미래"],"입니다.")
+# old 스타일
+print("%s 학생의 %s 과목 성적은 %s입니다." %(student,subject,subjects[subject]))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 모던 스타일
+print("{0} 학생의 {1} 과목 성적은 {2}입니다".format(student, subject ,subjects[subject])) #수정하자
+#  울트라 모던 스타일(f 스트링)
+print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
