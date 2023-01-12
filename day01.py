60d590dea09c0b15dfaac697e1decbae1dff8a3d
uminsong1512 <uminsong1512@gmail.com>
2023-01-12 17:58:58 +0900

    countdown 02

---
+++ day01.py
@@ -0,0 +1,34 @@
+# for countdown in 5,4,3,2,1,"hey":
+#     print(countdown)
+# print("프로그램 종료")
+# 단어_단어 : snake 표기법
+countdown_list=[5,4,3,2,1,"hey"]
+for countdown in countdown_list:
+    print(countdown)
+print(countdown_list[3]) #2
+print(countdown_list[-3]) #2
+print(countdown_list[-1]) #역방향 인덱싱
+print("프로그램 종료")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
