8085d8bbca39b6c0e123e194434171c38f3d10a2
uminsong1512 <uminsong1512@gmail.com>
2023-01-13 16:00:32 +0900

    mission02

--- day02_mission.py
+++ day02_mission.py
@@ -1,10 +1,18 @@
-#mission 1
-import random
-secret = random.randint(1,10)
-guess = int(input("1~10사이의 숫자를 입력하시오 :"))
-if guess < secret:
-    print("too low")
-elif guess > secret:
-    print("too high")
+#mission 2
+# 체리는 작고 녹색이 아니다
+# 완두콩은 작고 녹색이다
+# 수박은 크고 녹색이다
+# 호박은 크고 녹색이 아니다
+small = True
+green = True
+if small:
+    if green:
+        print("pea")
+    else:
+        print("cherry")
 else:
-    print("just right")
+    if green:
+        print("watermelon")
+    else:
+        print("pumpkin")
+
