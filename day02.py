10473a6150011ae9977078e6e8a9549551e80e27
uminsong1512 <uminsong1512@gmail.com>
2023-01-13 10:58:10 +0900

    day02 datatype float

--- day02.py
+++ day02.py
@@ -1,7 +1,5 @@
-harry_porter = {"헤르미온느": "그리핀도르","말포이" : "슬리데린","말포이" : "그리핀도르","론" : "그리핀도르"}
-print(harry_porter)
-print(type(harry_porter))
-
+PI=3.14
+print(f"원주율의 값은{PI}이고 타입은 {type(PI)}입니다.")
 
 
 
