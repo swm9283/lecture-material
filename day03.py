e247eda9b56baa13252996c249b32dd12108b06a
uminsong1512 <uminsong1512@gmail.com>
2023-01-16 13:42:57 +0900

    구구단예제2

--- day03.py
+++ day03.py
@@ -1,108 +1,15 @@
-# 반복문 : while과 for문
-gugudane =int(input("원하는 수의 구구단을 입력하세요, 예)3 :"))
-count = 1
-# while count<=9:
-#     print(f"{gugudane} * {count} = {gugudane*count}")
-#     count=count+1
-
-while count<=9:
-    print("{0} * {1} = {2}".format(gugudane,count,gugudane*count))
-    count=count+1
-
 while True:
-    gugudane = int(input("원하는 수의 구구단을 입력하세요, 예)3 :"))
+    gugudane = int(input("원하는 수의 구구단을 입력하세요,0을 입력시 프로그램이 종료됩니다., 예)3 :"))
     count = 1
+    if gugudane == 0:
+        print("프로그램이 종료되었습니다")
+        break
     # if gugudane>0 and gugudane<10: old style
     if 1 <= gugudane < 10:
         while count <= 9:
             print("{0} * {1} = {2}".format(gugudane, count, gugudane * count))
             count = count + 1
-        break
+
 
     else:
         print("1에서 9사이의 값을 입력하세요!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
