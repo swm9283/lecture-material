379ab0963642c1d905c370f7d649cc0cb01d31ce
uminsong1512 <uminsong1512@gmail.com>
2023-01-16 14:17:15 +0900

    구구단예제3, for문사용

--- day03.py
+++ day03.py
@@ -13,3 +13,90 @@ while True:
 
     else:
         print("1에서 9사이의 값을 입력하세요!")
+
+
+numbers = [1,3,5]
+position = 0
+while position <len(numbers):
+    number =numbers[position]
+    if number % 2 ==0:
+        print("found even number",number)
+        break
+    position = position +1
+else:
+    print("No even number found")
+
+
+#for문 사용
+while True:
+    gugudane = int(input("원하는 수의 구구단을 입력하세요,0을 입력시 프로그램이 종료됩니다., 예)3 :"))
+    if gugudane == 0:
+        print("프로그램이 종료되었습니다")
+        break
+    # if gugudane>0 and gugudane<10: old style
+    if 1 <= gugudane < 10:
+        for i in range(1,10,2): # 건너뛰기 사용가능
+            print("{0} * {1} = {2}".format(gugudane,i, gugudane * i))
+    else:
+        print("1에서 9사이의 값을 입력하세요!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
