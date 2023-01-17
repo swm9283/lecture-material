b9aa11381ff252a31d0f6b488d61bc82f48eddb5
uminsong1512 <uminsong1512@gmail.com>
2023-01-17 13:43:03 +0900

    day04 list comprehension and generator

--- day04.py
+++ day04.py
@@ -1,77 +1,27 @@
-#list
-#copy
-primes = [2,19,3.0,5,7,11]
-primes_cp=primes
-print(primes)
-print(primes_cp)
-primes[-1] = "lunch time"
-print(primes)
-print(primes_cp)
-primes_cp[0] = "morning coffe"
-print(primes)
-print(primes_cp)
+# list 03
+# list comprehension and generator
 
-#얕은 카피
-#같은 사물함을 사용하기 때문에 일어나는 문제이다.
-#참조의 형태일 경우 이 방식 주로 사용한다.
-# .copy() method list() 변환함수 슬라이스 [:]
-prac = [1,2,[3,2]]
-prac_copy = prac.copy()
-prac_list = list(prac)
-prac_slice = prac[:]
-print(prac,prac_copy,prac_list,prac_slice)
-prac[2][0] = 1
-prac[1] = -77 #immutable type이므로 영향을 받지 않는다.
-print(prac,prac_copy,prac_list,prac_slice)
+#list comprehension
+# [ 표현식 for 항목 in 순회 가능한 객체]
+# 책에 나온 여러 코드 작성해서 해보자.
 
+# 이 코드를 줄이고 싶다
+odd_list = []
+for i in range(1,11):
+    if i % 2 ==1 :
+        odd_list.append(i)
+print(odd_list)
 
+#리스트 컴프리헨션
+odd_list_comprehension= [i for i in range(1,11) if i % 2 ==1]
+print(odd_list_comprehension,type(odd_list_comprehension))
 
-#딥카피
-#명확하게 별도의 사본이 필요할 경우
-#
-import copy
-a = [1,2,[5,-9]]
-b=copy.deepcopy(a) #별도의 메모리 공간을 받은 것이다.
-a[2][1] = 7 #mutable, but deepcopy b is not affected
-print(a,b)
+#튜플 컴프리헨션이 없을까?
+# <generator object <genexpr> at 0x10488b510>
+#  튜플 컴프리헨션은 존재하지 않는다.
+odd_tupple =(i for i in range(1,11) if i % 2 ==1)
+print(odd_tupple,type(odd_tupple))
+
+#연습문제
 
 
-
-
-
-
-
-
-
-
-
-
-#  sort
-
-#  TypeError: '<' not supported between instances of 'str' and 'int'
-mixed = [6,4,5,"A",7,"트와이스","B","마마무"]
-mixed.sort()
-print(mixed)
-
-
-
-#문자형일 때 숫자 영어(대문자, 소문자) 한글 : 사전 순으로 정렬된다.
-mixed = ["6","4","5","A","7","트와이스","B","마마무","b"]
-# mixed.sort()
-mixed.sort(reverse=True) #  역순
-print(mixed)
-
-
-
-
-# primes = [2,19,3.0,5,7,11]
-# print(primes)
-# primes.sort()
-# print(primes)
-
-
-
-
-# primes_sorted = sorted(primes)
-# print(primes)
-# print(primes_sorted)
