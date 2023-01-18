464ce0a7f94d9630807af6c04836a78a73477a61
uminsong1512 <uminsong1512@gmail.com>
2023-01-18 12:52:15 +0900

    day05 def calculate_fee

--- day05.py
+++ day05.py
@@ -1,29 +1,31 @@
-# function
-# #input 2 numbers
-# # 두 수 사이의 소수만 출력하게끔 해주세요.
+#function
+def do_nothing():
+    pass
 
-#소수인지 판단하는 함수
-def isprime(n):
+# #None
+#
+# # print(do_nothing())
+#
+# mamamoo = ["화사","솔라","휘인","문별"]
+# # print(mamamoo.pop()) #삭제할 값 리턴 후 삭제. 문별 pop
+# print(mamamoo.remove("문별")) # 삭제만 함. 따라서 print 함수해서 출력할 값이 없다.
+# print(mamamoo)
+
+#매표소 요금 계산 for *
+# *args 가변적인 인수를 받을 때 용이하다. 즉 던져진 인수의 값을 예측할 수 없을 때 용이하다. 튜플로 묶어서 처리한다.
+# 튜플로 받기 때문에 가능하다.
+def calculate_fee(*args):
     """
-    매개변수로 받은 수가 소수인지 여부를 판단하는 함수
-    :param n: integer number
-    :return: retrun True or False
+    놀이공원 요금 계산 프로그림
+    :param args: ages
+    :return: total fee for paying
     """
-    if n <= 1:
-        return False
-    for k in range(2,n):
-        if n % k == 0:
-            return False
-    else:
-        return True
+    total = 0
+    for age in args:
+        if age >= 19:
+            total =  total + 10000
+        else:
+            total = total + 3000
+    return total
 
-print(isprime(100))
-help(isprime) # 내가 작성한 코드의 도움말 확인!
-
-start = int(input(" enter the start number : "))
-end = int(input("enter the end number : "))
-if end < start:
-    start, end = end, start
-for i in range(start, end+1):
-    if isprime(i):
-        print(i, end=" ")
+print(calculate_fee(45,43,10,7))
