140e22823a0243eb12f4af86af11406a3d789505
uminsong1512 <uminsong1512@gmail.com>
2023-01-18 16:24:14 +0900

    day05 function lambda

--- day05.py
+++ day05.py
@@ -1,25 +1,33 @@
-#function
-#함수를 인수로 받아서 매개변수로 던질 수 있다.
-#함수 안에 함수를 또 정의할 수 있다.(inner function)
-#closure nolocal local variable에 대한 공부!
-#closure 매우 중요한 개념이다. 여러번 봐보자
-def calculate():
-    x = 1
-    y = 2
-    temp = 0
+# function
+#lambda
 
-    def add_sub(n):
-        # nonlocal x #nonlcal을 사용하면 지역변수를 변경할 수 있다.
-        nonlocal temp #calculate 내의 temp와 add_sub의 temp가 같다.
-        # x = 11 # local variable
-        temp = temp + x + n - y  # add_sub가 x,y를 기억하고 있다.
-        return temp
-    print("once")
-    return add_sub #합수 이름이 return값.
+#function to use parameter
+import random
 
-c1 = calculate()
+# def squares(n):
+#     """
+#     제곱 함수
+#     :param n: integer
+#     :return:  integer
+#     """
+#     return n **2
+#
+# def process(no_lists,f):
+#     for no in no_lists:
+#         print(f(no))
+#
+#
+# numbers = [random.randint(1,100) for i in range(5)]
+# print(numbers)
+# process(numbers,squares)
 
-for i in range(5):
-    print(c1(i)) # add_sub에 i를 넣은 것과 같다. calculate는 한 번 돌아가는데 그 와중에 add_sub가 5번 돌아가네
-print(type(c1))
-print(c1)
+#function lambda
+# 함수 이름이 들어가는 자리에 lambda 함수 이렇게 짧고 간결하게 작성할 수 있다.
+def process(no_lists,f):
+    for no in no_lists:
+        print(f(no))
+
+
+numbers = [random.randint(1,100) for i in range(5)]
+print(numbers)
+process(numbers, lambda x: x*x)
