ed503ac2078cb302bb3c11ba51c50daaf5531fab
uminsong1512 <uminsong1512@gmail.com>
2023-01-19 15:07:53 +0900

    day06 function example

--- day06.py
+++ day06.py
@@ -1,31 +1,19 @@
-# try except
+# ex 9-1
+def good():
+    return ["Harry", "Ron", "Hermione"]
+print(good())
 
-
-# def div_calc(a,b):
-#     """
-#     나누기 함수
-#     :param a: 분자
-#     :param b: 분모
-#     :return:  계산결과
-#     """
-#     return a/b
-# print(div_calc(15,3))
-# print(div_calc(15,0))
-
-try:
-#    raise Exception("쉬는 시간") #강제로 예외를 던지는 것이다.
-#     raise TypeError("타입 에러")
-    expr = input("분자 분모 입력(띄어쓰기로 구분) : ").split()
-    print(int(expr[0]) / int(expr[1]))
-except ValueError as err:
-    print(f"숫자를 입력해주세요. {err}")
-except ZeroDivisionError as err:
-    print(f"분모에 0이 올 수 없습니다. {err} ")
-except IndexError as err:
-    print(f"입력 값의 범위에 문제가 있습니다. {err}")
-except Exception as other:
-    print(f"예외 발생! {other}")
-else: #예외가 발생 하지 않았을 때 ,정상적으로 코드가 마쳤을 때 실행
-    print("프로그램 정상", end=" ") #end가 뭘까? 다시 공부하자...
-finally: #예외 발생 여부와 상관없이 무조건 실행
-    print("종료")
+# ex 9-2
+def get_odds():
+    """
+    range(10) 중에서 홀수만 generator한다.
+    :return: generator
+    """
+    for i in range(10):
+        # if i % 2: # 0이 아닌 수는 모두 True이다.
+        if i % 2 == 1:
+            yield i
+temp = []
+for k in get_odds():
+    temp.append(k)
+print(temp[2])
