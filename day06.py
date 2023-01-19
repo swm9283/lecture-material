3b659d91ae50d26fc666721177af2161f2502995
uminsong1512 <uminsong1512@gmail.com>
2023-01-19 14:04:17 +0900

    day06 functin try and except

--- day06.py
+++ day06.py
@@ -1,29 +1,31 @@
-# recursion 재귀함수
-def factorial_iter(n):
-    """
-    반복문을 사용한 팩토리얼 함수
-    :param n: n!
-    :return: integer 팩토리얼 게싼 결과 값.
-    """
-    result = 1
-    for k in range(1,n+1):
-        result = result * k
-    return result
-print(factorial_iter(5))
-
-def factorial_recu(n):
-    """
-    재귀 함수를 이용한 팩토리얼 함수
-    :param n: n!
-    :return:  자기 자신을 호출 또는 1
-    """
-    if n == 1: # 끝나는 조건
-        return 1
-    else:
-        return factorial_recu(n-1) * n # factorial_recu가 (n-1)!이라는 의미를 내포하고 있다.
-
-print(factorial_iter(1000))
-print(factorial_recu(1000))
+# try except
 
 
+# def div_calc(a,b):
+#     """
+#     나누기 함수
+#     :param a: 분자
+#     :param b: 분모
+#     :return:  계산결과
+#     """
+#     return a/b
+# print(div_calc(15,3))
+# print(div_calc(15,0))
 
+try:
+#    raise Exception("쉬는 시간") #강제로 예외를 던지는 것이다.
+#     raise TypeError("타입 에러")
+    expr = input("분자 분모 입력(띄어쓰기로 구분) : ").split()
+    print(int(expr[0]) / int(expr[1]))
+except ValueError as err:
+    print(f"숫자를 입력해주세요. {err}")
+except ZeroDivisionError as err:
+    print(f"분모에 0이 올 수 없습니다. {err} ")
+except IndexError as err:
+    print(f"입력 값의 범위에 문제가 있습니다. {err}")
+except Exception as other:
+    print(f"예외 발생! {other}")
+else: #예외가 발생 하지 않았을 때 ,정상적으로 코드가 마쳤을 때 실행
+    print("프로그램 정상", end=" ") #end가 뭘까? 다시 공부하자...
+finally: #예외 발생 여부와 상관없이 무조건 실행
+    print("종료")
