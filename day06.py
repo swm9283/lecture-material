fd952964a4a5afbb308d67f3aa6b03e4f554f0b1
uminsong1512 <uminsong1512@gmail.com>
2023-01-19 15:40:42 +0900

    day06 class

--- day06.py
+++ day06.py
@@ -1,19 +1,31 @@
-# ex 9-1
-def good():
-    return ["Harry", "Ron", "Hermione"]
-print(good())
+# class Pokemon:
 
-# ex 9-2
-def get_odds():
-    """
-    range(10) 중에서 홀수만 generator한다.
-    :return: generator
-    """
-    for i in range(10):
-        # if i % 2: # 0이 아닌 수는 모두 True이다.
-        if i % 2 == 1:
-            yield i
-temp = []
-for k in get_odds():
-    temp.append(k)
-print(temp[2])
+#     def __init__(self): # 객체 생성 시 동작.
+#         print("포켓몬 객체 생성됨.")
+#
+#
+# p1 = Pokemon()
+# p2 = Pokemon()
+# print(p1,p2)
+# print(id(p1))
+# print(id(p2))
+
+class Pokemon:
+    def __init__(self, name, owner, skills): # 객체 생성 시 동작.
+        self.name = name
+        self.owner = owner
+        self.skills = skills.split("/")
+        print(f"포켓몬 {name} 생성됨.")
+    def info(self):
+        print(f"{self.owner}의 포켓몬은 {self.name}입니다")
+        print("사용할 수 있는 skill은")
+        for skill in self.skills:
+            print(skill)
+
+
+p1 = Pokemon("피카츄", "한지우","50만 볼트/100만 볼트/번개")
+p2 = Pokemon("꼬부기", "오바람","고속스핀/거품/몸통박치기/하이드로펌프")
+p1.info()
+p2.info()
+# print(f"{p1.owner}의 포켓몬은 {p1.name}입니다")
+# print(f"{p2.owner}의 포켓몬은 {p2.name}입니다")
