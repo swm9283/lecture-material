8ab9108a5c16646f7d11fdef3cbd7e86262bbe84
uminsong1512 <uminsong1512@gmail.com>
2023-01-19 10:36:41 +0900

    day06 function decorator

--- day06.py
+++ day06.py
@@ -1,30 +1,21 @@
-#generator
+# decorator
+#decorator
+def document_info(func):
+    def new_function(*args, **kwargs):
+        print("Running function:", func.__name__)
+        print("Positional arguments:", args)
+        print("Keyword arguments:", kwargs)
+        result = func(*args,**kwargs)
+        print("Result:", result)
+        return result
+    return new_function
 
-#대표적인 예는 range() function이다. 이 방식으로 이해하면 쉽다.
-def a(): # generator
-    yield 1
-    yield 2
-    yield 3
+def sub_int(x,y):
+    return  x - y
 
-def b(): #normal function
-    return 1 # function stop
-    return 2 #죽은 코드
-    return 3 #죽은 코드
+print(sub_int(7,3))
 
-print(a(),b())
-for i in a():
-    print(i)
-
-c = a()
-print(c)
-for i in c:
-    print(i)
-
-for i in c: # generator는 한 번만 순회할 수 있다.
-    print(i)
-
-#zip에 대해 공부하자.
-a = list(zip(["a","b"],["1","2"]))
-print(a)
-print(type(a[0]))
-
+#수동방식
+info_sub_int = document_info(sub_int)
+r = info_sub_int(7,3)
+print(r)
