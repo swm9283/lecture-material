db3700c83a5a1884693d02c2fc17048fe8378230
uminsong1512 <uminsong1512@gmail.com>
2023-02-03 11:30:34 +0900

    day15 linear list 03

--- day15.py
+++ day15.py
@@ -13,9 +13,31 @@ def insert_data(position, pokemon):
 
     pokemons[position] = pokemon  # 지정한 위치에 친구 추가
 
-if __name__ == "__main__":
+
+def delete_data(position):
+    if position < 0 or position > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[position] = None  # 데이터 삭제
+
+    for i in range(position + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[len_pokemons - 1])
+
+
+if __name__ == "__main__": # main 함수?
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+    print(pokemons)
     insert_data(2, '거북왕')
+    # pokemons.insert(2,"거북왕")  built-in 코드
     print(pokemons)
     insert_data(6, '망나뇽')
+    # pokemons.insert(6,"망나뇽")  built-in 코드
+    print(pokemons)
+
+    delete_data(1)
     print(pokemons)
