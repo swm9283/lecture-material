7c9f48965e6c55322664f0bac1de8eaa87f62b09
uminsong1512 <uminsong1512@gmail.com>
2023-02-03 10:48:34 +0900

    day15 linear list 01

---
+++ day15.py
@@ -0,0 +1,15 @@
+pokemons = []  # 빈 배열
+
+
+def add_data(pokemon):
+    pokemons.append(None) # 공간 확보
+    # pokemons[len(pokemons) - 1] = friend  #맨 끝에 추가.
+    pokemons[-1] = pokemon
+
+add_data('피카츄')
+add_data('라이츄')
+add_data('파이리')
+add_data('꼬부기')
+add_data('이상해')
+
+print(pokemons)
